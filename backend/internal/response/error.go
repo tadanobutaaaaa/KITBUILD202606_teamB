660257f7f1efad914d46330c5cf61d954aafa41d
tadanobutaaaaa <youyah1004@gmail.com>
2026-06-28 02:19:27 +0900

    カテゴリ、商品、店舗のハンドラーで成功レスポンスを追加し、エラーレスポンスのフォーマットを修正

--- backend/internal/response/error.go
+++ backend/internal/response/error.go
@@ -6,8 +6,7 @@ import (
 
 func ErrorResponse(r *gin.Context, http_status_code int, message string, err error) {
 	r.JSON(http_status_code, gin.H{
-		"http_status_code": http_status_code,
-		"message":          message,
-		"error":            err,
+		"message": message,
+		"error":   err,
 	})
 }
