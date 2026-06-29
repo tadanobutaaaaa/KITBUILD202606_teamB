43182fdd98fb5c1935cbd2a264bd7a4fbf8e0dbe
takumireid <legottmax@gmail.com>
2026-06-29 22:19:22 +0900

    fix

---
+++ backend/handlers/store_handler.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// 店舗一覧表示
+func GetStoreList(c *gin.Context) {
+
+}
+
+// 店舗個別表示
+func GetStore(c *gin.Context) {
+
+}
+
+// 店舗新規作成
+func CreateStore(c *gin.Context) {
+
+}
+
+// 店舗情報更新
+func UpdateStore(c *gin.Context) {
+
+}
+
+// 店舗削除
+func DeleteStore(c *gin.Context) {
+
+}
---
+++ backend/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/tadanobutaaaaa/KITBUILD202606_teamB/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	//店舗のCRUDエンドポイント
+	store := r.Group("/store")
+	{
+		store.GET("", handlers.GetStoreList)
+		store.GET("/:id", handlers.GetStore)
+		store.POST("", handlers.CreateStore)
+		store.PATCH("/:id", handlers.UpdateStore)
+		store.DELETE("/:id", handlers.DeleteStore)
+	}
+
+	//カテゴリーのCRUDエンドポイント
+	category := r.Group("/category")
+	{
+		category.GET("", handlers.GetCategoryList)
+		category.GET("/:id", handlers.GetCategory)
+		category.POST("", handlers.CreateCategory)
+		category.PATCH("/:id", handlers.UpdateCategory)
+		category.DELETE("/:id", handlers.DeleteCategory)
+	}
+
+	//商品のCRUDエンドポイント
+	product := r.Group("/product")
+	{
+		product.GET("", handlers.GetProductList)
+		product.GET("/:id", handlers.GetProduct)
+		product.POST("", handlers.CreateProduct)
+		product.PATCH("/:id", handlers.UpdateProduct)
+		product.DELETE("/:id", handlers.DeleteProduct)
+	}
+
+	r.Run() // デフォルトでは localhost:8080 で起動
+}
